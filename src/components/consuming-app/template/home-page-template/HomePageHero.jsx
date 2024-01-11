b119a193d345952febd42b74227247ee3c33d458
Jonathan Lassiter <36551695+LassiterJ@users.noreply.github.com>
2024-01-12 04:28:22 +1100

    Added logic to set the height of the Hero component. This value is calculated to be 100vh-headerHeight. The header height is given by the appData. Resizing of the viewport dynamically changes the size of the hero component.

---
+++ src/components/consuming-app/template/home-page-template/HomePageHero.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {useAppData} from '../../smart/app-data/AppData';
+import styles from './HomePageTemplate.module.scss';
+export const HomePageHero = () => {
+  const { appData } = useAppData();
+  
+  const heroStyle = appData?.header?.height ? {
+    height: `calc(100vh - ${appData.header.height}px)`
+  }:{};
+  
+  return (
+    <div className={styles.HeroContainer} style={heroStyle}> {/* Has background image. TODO: Make Container Component (Layout)*/}
+      {/*<div className={'hero-box-spacer'}> /!* May not be necessary in code.*!/*/}
+      {/*</div>*/}
+      {/*<div className={'hero-overlayed-content'}>*/}
+      {/*  <div className={'hero-box-cta-row'}> /!* TODO: Make a Row/stack Component (Layout)*!/*/}
+      {/*    <div className={'hero-link-card'}> /!* TODO: Make a Link Card Component (Concrete) *!/*/}
+      {/*    </div>*/}
+      {/*    <div className={'hero-link-card'}>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*  <div className={'hero-headline'}>*/}
+      {/*    /!*<h2>Fast, friendly, and simple stay in your car service.</h2>*!/*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+    </div>
+  );
+};
--- src/components/consuming-app/template/home-page-template/HomePageTemplateLayoutExample.jsx
+++ src/components/consuming-app/template/home-page-template/HomePageTemplateLayoutExample.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AspectRatio} from '@radix-ui/react-aspect-ratio';
 import styles from "./HomePageTemplate.module.scss";
+import { HomePageHero } from './HomePageHero';
 
 
 // Content on this page is for demonstration purposes only.
@@ -12,21 +13,8 @@ export const HomePageTemplateLayoutExample = ({children}) => {
   return(
     <>
       <section className={styles.HeroSection} style={{ backgroundColor: "yellowgreen"}}>
-        {/*<div className={styles.HeroContainer}> /!* Has background image. TODO: Make Container Component (Layout)*!/*/}
-        {/*  <div className={'hero-box-spacer'}> /!* May not be necessary in code.*!/*/}
-        {/*  </div>*/}
-        {/*  <div className={'hero-overlayed-content'}>*/}
-        {/*    <div className={'hero-box-cta-row'}> /!* TODO: Make a Row/stack Component (Layout)*!/*/}
-        {/*      <div className={'hero-link-card'}> /!* TODO: Make a Link Card Component (Concrete) *!/*/}
-        {/*      </div>*/}
-        {/*      <div className={'hero-link-card'}>*/}
-        {/*      </div>*/}
-        {/*    </div>*/}
-        {/*    <div className={'hero-headline'}>*/}
-        {/*      /!*<h2>Fast, friendly, and simple stay in your car service.</h2>*!/*/}
-        {/*    </div>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
+        <HomePageHero />
+        
       </section>
       <section style={{height: "20rem",backgroundColor: "lightgray"}}>
         {/*<h4>Find Your T5 Section</h4>*/}
