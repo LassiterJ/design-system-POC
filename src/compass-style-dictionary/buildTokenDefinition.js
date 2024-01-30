1c0638de52e3bef37a417ea80d4c069c7f89c182
Jonathan Lassiter <36551695+LassiterJ@users.noreply.github.com>
2024-01-29 19:24:48 -0500

    Closer to solving StyleDictionary output problem. Added ThemeMerger utility class to handle Theme Merges and outmut.

--- src/compass-style-dictionary/buildTokenDefinition.js
+++ src/compass-style-dictionary/buildTokenDefinition.js
@@ -8,25 +8,100 @@
 //   name:	String (optional)	Usually the name for a design token is generated with a name transform, but you can write your own if you choose. By default Style Dictionary will add a default name which is the key of the design token object.
 //   attributes:	Object (optional)
 
-const buildTokenName = ({key="", keyFormatter=(key)=>key, prefix="", suffix=""}) => {
-  const formattedKey = keyFormatter(key);
-  return `${prefix}${prefix && "-"}${formattedKey}${suffix && "-"}${suffix}`;
+
+// const buildTokenName = ({key="", keyFormatter=(key)=>key, prefix="", suffix=""}) => {
+//   const formattedKey = keyFormatter(key);
+//   return `compass.${prefix}${prefix && "."}${formattedKey}${suffix && "."}${suffix}`;
+// }
+// const buildToken = ({key="", value="", keyFormatter=(key)=>key, valueFormatter=(value)=>value, prefix="", suffix=""}) => {
+//   const tokenName = buildTokenName({key, keyFormatter, prefix, suffix});
+//   const formattedValue = valueFormatter({value});
+//   return {[tokenName]: formattedValue};
+// };
+// const buildTokensFromScale = ({ scale= null , keyFormatter=(key)=>key, valueFormatter=(value)=>value, prefix="", suffix="", exclude: excludeKey = []}) => {
+//   if(!scale){
+//     console.error("buildTokensFromScale: scale is undefined");
+//     return null;
+//   };
+//   // TODO: Add some sort of mapping to allow for keys to be mapped to a different value than scale[key]
+//   return Object.keys(scale).reduce((tokens, key) => {
+//     if(excludeKey.includes(key)) return tokens;
+//     return Object.assign({}, tokens, buildToken({key, value: scale[key], keyFormatter, valueFormatter, prefix, suffix}));
+//   }, {});
+// };
+
+const deepMerge = (...objects) => {
+  const isObject = obj => obj && typeof obj === 'object';
+  
+  return objects.reduce((prev, obj) => {
+    Object.keys(obj).forEach(key => {
+      const pVal = prev[key];
+      const oVal = obj[key];
+      
+      if (Array.isArray(pVal) && Array.isArray(oVal)) {
+        prev[key] = pVal.concat(...oVal);
+      }
+      else if (isObject(pVal) && isObject(oVal)) {
+        prev[key] = deepMerge(pVal, oVal);
+      }
+      else {
+        prev[key] = oVal;
+      }
+    });
+    
+    return prev;
+  }, {});
 }
-const buildToken = ({key="", value="", keyFormatter=(key)=>key, valueFormatter=(value)=>value, prefix="", suffix=""}) => {
-  const tokenName = buildTokenName({key, keyFormatter, prefix, suffix});
-  const formattedValue = valueFormatter({value});
-  return {[tokenName]: formattedValue};
+const splitAndFormatKey = (key, prefix) => {
+  let parts = key.includes('/') ? key.split('/').map(k => k.trim()) : [key];
+  return [...prefix.split('-'), ...parts].filter(Boolean);
 };
-const buildTokensFromScale = ({ scale= null , keyFormatter=(key)=>key, valueFormatter=(value)=>value, prefix="", suffix="", exclude: excludeKey = []}) => {
-  if(!scale){
-    console.error("buildTokensFromScale: scale is undefined");
-    return null;
+
+const buildNestedObject = (path, value, obj) => {
+  path.reduce((acc, part, index) => {
+    if (index === path.length - 1) {
+      acc[part] = { value };
+    } else {
+      acc[part] = acc[part] || {};
+    }
+    return acc[part];
+  }, obj);
+};
+
+const buildTokensFromScale = ({scale, keyFormatter = (key) => key, valueFormatter = (value) => value, prefix, exclude = []}) => {
+  console.log("buildTokensFromScale");
+  if (!scale) {
+    throw new Error("buildTokensFromScale: scale is undefined");
+  }
+  
+  const formattedPrefix = `compass-${prefix}`;
+  
+  const scaleKeys = Object.keys(scale);
+  console.log("scaleKeys: ", scaleKeys);
+  const tokens = {};
+  
+  // iterate through each key in the scale and build a token for it
+  for(const keys of scaleKeys) {
+    // if the key is in the exclude list, skip it
+    if (exclude.includes(keys)) continue;
+  // otherwise, format the key and value and add it to the tokens object
+    const formattedKey = keyFormatter(keys);
+    const formattedValue = valueFormatter(scale[keys]);
+    const tokenPath = splitAndFormatKey(formattedKey, formattedPrefix);
   };
   
+  
+  
   return Object.keys(scale).reduce((tokens, key) => {
-    if(excludeKey.includes(key)) return tokens;
-    return Object.assign({}, tokens, buildToken({key, value: scale[key], keyFormatter, valueFormatter, prefix, suffix}));
+    if (exclude.includes(key)) return tokens;
+    
+    const formattedKey = keyFormatter(key);
+    const tokenPath = splitAndFormatKey(formattedKey, formattedPrefix);
+    const formattedValue = valueFormatter(scale[key]);
+    buildNestedObject(tokenPath, formattedValue, tokens);
+    return tokens;
   }, {});
 };
 
-module.exports = {buildToken, buildTokensFromScale, buildTokenName};
+
+module.exports = {buildTokensFromScale};
--- src/compass-style-dictionary/tokens/primitive/margin.js
+++ src/compass-style-dictionary/tokens/primitive/margin.js
@@ -1,61 +1,71 @@
-// import {coreSpacingScale, fractionalSpacingScale, coreSpacingTokens, fractionalSpacingTokens} from './spacing.js';
-// import { buildToken, buildTokensFromScale } from '../../../utilities/js/buildTokenDefinition.js';
+// // const {coreSpacingScale, fractionalSpacingScale, coreSpacingTokens, fractionalSpacingTokens} = require('./spacing.js');
+const {coreSpacingScale, fractionalSpacingScale} = require('./scales.js');
+const { buildToken, buildTokensFromScale } = require( '../../buildTokenDefinition.js');
 //
 // console.log("coreSpacingTokens: ", coreSpacingTokens);
 // // exports marginTokens that look like:
 // // {
-// //  "compass-m-0": {value: {compass.spacing.0.value}},
+// //  "m-0": {value: {compass.spacing.0.value}},
 //
 // // Need to find way to map references to other tokens.
-// const buildMarginTokens = ({ prefix }) => {
-//   const coreSpacingTokens = buildTokensFromScale({
-//     scale: coreSpacingScale,
-//     keyFormatter: (key) => key.replace(".", "_"),
-//     valueFormatter: (value) => `${value},
-//     prefix: prefix
-//   });
-//   const fractionalSpacingTokens = buildTokensFromScale({
-//     scale: fractionalSpacingScale,
-//     keyFormatter: (key) => key.replace("/", "--"),
-//     prefix: prefix
-//   });
-//   const negativeCoreSpacingTokens = buildTokensFromScale({
-//     scale: coreSpacingScale,
-//     keyFormatter: (key) => key.replace(".", "_"),
-//     valueFormatter: (value) => `-${value}`,
-//     prefix: `-${prefix}`,
-//     exclude: ["0"]
-//   });
-//   const negativeFractionalSpacingTokens = buildTokensFromScale({
-//     scale: fractionalSpacingScale,
-//     keyFormatter: (key) => key.replace("/", "--"),
-//     valueFormatter: (value) => `-${value}`,
-//     prefix: `-${prefix}`
-//   });
-//   const autoToken = buildToken({ key: "auto", value: "auto", prefix: "compass-spacing" });
-//
-//   return {
-//     ...coreSpacingTokens,
-//     ...fractionalSpacingTokens,
-//     ...negativeCoreSpacingTokens,
-//     ...negativeFractionalSpacingTokens,
-//     ...autoToken}
-// };
-// const marginTokens = buildMarginTokens({ prefix: "compass-m" });
-// const marginXTokens = buildMarginTokens({ prefix: "compass-mx" });
-// const marginYTokens = buildMarginTokens({ prefix: "compass-my" });
-// const marginTopTokens = buildMarginTokens({ prefix: "compass-mt" });
-// const marginStartTokens = buildMarginTokens({ prefix: "compass-ms" });
-// const marginBottomTokens = buildMarginTokens({ prefix: "compass-mb" });
-// const marginEndTokens = buildMarginTokens({ prefix: "compass-me" });
-// const marginPropDefs = {
-//     ...marginTokens,
-//     ...marginXTokens,
-//     ...marginYTokens,
-//     ...marginTopTokens,
-//     ...marginStartTokens,
-//     ...marginBottomTokens,
-//     ...marginEndTokens
-// };
+const buildMarginTokens = ({ prefix }) => {
+  // const coreSpacingTokens = buildTokensFromScale({
+  //   scale: coreSpacingScale,
+  //   keyFormatter: (key) => key.replace(".", "_"),
+  //   valueFormatter: (value) => `${value},
+  //   prefix: prefix
+  // });
+
+  const fractionalSpacingTokens = buildTokensFromScale({
+    scale: fractionalSpacingScale,
+    keyFormatter: (key) => key.replace("/", "--"),
+    prefix: prefix
+  });
+  // const negativeCoreSpacingTokens = buildTokensFromScale({
+  //   scale: coreSpacingScale,
+  //   keyFormatter: (key) => key.replace(".", "_"),
+  //   valueFormatter: (value) => `-${value}`,
+  //   prefix: `-${prefix}`,
+  //   exclude: ["0"]
+  // });
+  const negativeFractionalSpacingTokens = buildTokensFromScale({
+    scale: fractionalSpacingScale,
+    // keyFormatter: (key) => key.replace("/", "--"),
+    valueFormatter: (value) => `{spacing-f-${value}}`,
+    prefix: `-${prefix}`
+  });
+  // const autoToken = buildToken({ key: "auto", value: "auto", prefix: "spacing" });
+
+  return {
+    // ...coreSpacingTokens,
+    ...fractionalSpacingTokens,
+    // ...negativeCoreSpacingTokens,
+    ...negativeFractionalSpacingTokens,
+    // ...autoToken
+  }
+};
+const marginTokens = buildMarginTokens({ prefix: "m" });
+const marginXTokens = buildMarginTokens({ prefix: "mx" });
+const marginYTokens = buildMarginTokens({ prefix: "my" });
+const marginTopTokens = buildMarginTokens({ prefix: "mt" });
+const marginStartTokens = buildMarginTokens({ prefix: "ms" });
+const marginBottomTokens = buildMarginTokens({ prefix: "mb" });
+const marginEndTokens = buildMarginTokens({ prefix: "me" });
+const marginPropDefs = {
+    ...marginTokens,
+    ...marginXTokens,
+    ...marginYTokens,
+    ...marginTopTokens,
+    ...marginStartTokens,
+    ...marginBottomTokens,
+    ...marginEndTokens
+};
 // // Values are wrong. Should be corresponding spacing token reference. not raw values.
-// console.log("marginPropDefs: ", marginPropDefs);
+console.log("marginTokens: ", marginTokens);
+//
+
+module.exports = {
+  'm-f-1-2': { value: '{compass.spacing.f.1.2}' },
+// ...marginTokens
+};
+//
