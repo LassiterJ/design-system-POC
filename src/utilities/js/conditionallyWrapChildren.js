11e7d402d57abd2ed26d8bda4b538a45fc2bcae4
Jonathan Lassiter <36551695+LassiterJ@users.noreply.github.com>
2024-02-26 11:54:56 -0500

    modified README to include npm token info for private packages

---
+++ src/utilities/js/conditionallyWrapChildren.js
@@ -0,0 +1,15 @@
+/**
+ * Facilitates correct DOM nesting for components using `asChild` with `Slot`.
+ * This helper ensures that when the `asChild` prop is utilized, the resulting DOM structure
+ * correctly integrates consumer-specified `children`, preserving intended order and nesting.
+ * It supports both direct content or a function returning content as the second argument.
+ */
+export const getSubtree = (options, content) => {
+  const { asChild, children } = options;
+  if (!asChild) return typeof content === 'function' ? content(children) : content;
+
+  const firstChild = React.Children.only(children);
+  return React.cloneElement(firstChild, {
+    children: typeof content === 'function' ? content(firstChild.props.children) : content,
+  });
+};
